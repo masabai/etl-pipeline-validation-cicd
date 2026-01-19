31c961106938d1ea9c92afe28d1bd20be23b254b
MAye <winthutaye@gmail.com>
2026-01-19 00:22:10 +0000

    update transform

--- etl/transform.py
+++ etl/transform.py
@@ -18,7 +18,6 @@ def load_txt_files(raw_dir):
         logging.info(f"Loaded {txt_file.name} → {txt_file.stem}")
     return dfs
 
-
 # ------------------ Cleaning Functions ------------------ #
 def clean_common_fields(df):
     df = df.copy()
@@ -35,7 +34,7 @@ def clean_common_fields(df):
 
     # Fill missing strings
     for col in df.select_dtypes(include="object").columns:
-        df[col] = df[col].fillna("")
+        df[col] = df[col].fillna("Unknown")
 
     # Normalize IDs
     if "primaryid" in df.columns:
